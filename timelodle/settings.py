30afa5fe47e646147592adde7f027c5017a0cfb8
vkozkan <vedatkemalozkan@gmail.com>
2025-05-27 15:56:51 -0400

    req6

--- timelodle/settings.py
+++ timelodle/settings.py
@@ -30,6 +30,15 @@ ALLOWED_HOSTS = [
     'localhost',  # optional for local testing
 ]
 
+import os
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Optional: if using collectstatic
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Application definition
 
@@ -122,7 +131,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
